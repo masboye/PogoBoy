6b8a36029bb155598f5fccab8dfb0a9c6725aeef
boy.setiawan <boye.setiawan@gmail.com>
2019-06-02 11:37:43 +0700

    done setting background

--- Telur/GameScene.swift
+++ Telur/GameScene.swift
@@ -11,5 +11,39 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let backgroundNode = SKSpriteNode(imageNamed: "background")
+    let secondBackgroundNode = SKSpriteNode(imageNamed: "second")
+    let bottomNode = SKSpriteNode(imageNamed: "bottom")
     
+    let foregroundNode = SKSpriteNode()
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        scene?.anchorPoint = CGPoint(x: 0, y: 0)
+        backgroundColor = SKColor(ciColor: .white)
+        
+        //addBackground()
+        addSecondBackground()
+        addBottom()
+    }
+    
+    func addBackground(){
+        // adding the background
+        backgroundNode.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        addChild(backgroundNode)
+        
+    }
+    func addSecondBackground(){
+        // adding the second background
+        secondBackgroundNode.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        //secondBackgroundNode.alpha = 1
+        addChild(secondBackgroundNode)
+        
+    }
+    func addBottom(){
+        // adding the background
+        bottomNode.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        addChild(bottomNode)
+        
+    }
 }
